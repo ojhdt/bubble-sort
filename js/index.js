04973cf1f62ccc2afc90b9af8dff17c93bbb7699
ojhdt <ojhdtmail@gmail.com>
2021-01-06 00:48:43 +0800

    update

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 const numset = document.getElementById("textfield");
-
+const sortshow = document.getElementById("displayfield");
+const slider = document.getElementById("slider");
 var Array = [];
 
 var Rand = function(){
@@ -7,7 +8,7 @@ var Rand = function(){
 }
 
 var Getnum = function(){
-    let num = document.getElementById("slider").value;
+    let num = slider.value;
     return num;
 }
 
@@ -22,11 +23,16 @@ var Fresh = function(){
 }
 
 var Confirm = function(){
+    slider.setAttribute("disabled","");
+    mdui.updateSliders();
     let nums = document.querySelectorAll('#textfield input');
     for(let[index, num] of nums.entries())
     {
         Array[index] = num.value;
+        num.setAttribute("disabled","");
     }
+    mdui.mutation();
+    Sort();
 }
 
 var Random = function(){
@@ -34,9 +40,30 @@ var Random = function(){
     for(let[index, num] of nums.entries())
     {
         num.value = Rand();
-    }  
+    }
 }
 
-var Retry = function(){
+var Sort = function(){
+    for(i = 0; i < Getnum() - 1; i++)
+    {
+        for(j = 0; j < Getnum() - 1; j++)
+        {
+            if(Array[j] < Array[j+1])
+            {
+                temp = Array[j];
+                Array[j] = Array[j+1];
+                Array[j+1] = temp;
+            }
+        }
+    }
+}
 
+var Display = function(){
+    sortshow.innerHTML = "";
+    for(i = 1; i <= Getnum(); i++)
+    {
+        let old_content = sortshow.innerHTML;
+        let new_content = "<div class='mdui-textfield mdui-textfield-floating-label'><label class='mdui-textfield-label'>" + "num" + i + "</label><input type='text' class='mdui-textfield-input'></div>";
+        sortshow.innerHTML = old_content + new_content;
+    }
 }
