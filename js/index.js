8dd65dec1d7904577434a389ae5e196c621406ff
ojhdt <ojhdtmail@gmail.com>
2021-01-06 19:48:11 +0800

    final update

--- js/index.js
+++ js/index.js
@@ -5,6 +5,7 @@ const logset = document.getElementById("logfield");
 const result = document.getElementById("result");
 var Array = [];
 var doing = 0;
+var docut = 0;
 
 var inst = new mdui.Dialog('#dialog');
 
@@ -34,6 +35,7 @@ var Fresh = function(){
 
 var Confirm = function(){
     window.scrollTo(0,0);
+    result.setAttribute("style", "display:none");
     document.getElementById("btn1").setAttribute("disabled","");
     document.getElementById("btn2").setAttribute("disabled","");
     slider.setAttribute("disabled","");
@@ -46,31 +48,61 @@ var Confirm = function(){
     }
     mdui.mutation();
     //Sort();
-    var times = (Getnum()-1)*(Getnum()-1)*1000;
+    var times = Getnum()*(Getnum()-1)*500;
     var interval1 = window.setInterval("Swap()", 1000);
     setTimeout(function() {window.clearInterval(interval1);Done();}, times);
 }
 
 var Swap = function(){
-    var j = this.doing % (Getnum()-1);
-    let is_change = Array[j] < Array[j+1] ? "swapped" : "needn't swap";
+    var j = this.doing;
+    // - Math.floor((this.doing / (Getnum()-1)))
+    let is_change = Number(Array[j]) < Number(Array[j+1]) ? "swapped" : "needn't swap";
     let old_content = logset.innerHTML;
-    let new_content = "Comparing "+ Array[j] + " and " + Array[j+1] + ", " + is_change + "</br>";
+    let new_content = "Comparing "+ Array[j] + " with " + Array[j+1] + ", " + is_change + "</br>";
     logset.innerHTML = new_content + old_content;
-    if(Array[j] < Array[j+1])
+    if(Number(Array[j]) < Number(Array[j+1]))
     {
         temp = Array[j];
         Array[j] = Array[j+1];
         Array[j+1] = temp;
     }
     Display();
-    this.doing++;
+    if(this.doing < (Getnum() - 2 - this.docut))
+    {
+        this.doing++;
+    }
+    else
+    {
+        this.doing = 0;
+        this.docut++;
+    }
 }
 
 var Done = function(){
     inst.open();
     this.doing = 0;
+    this.docut = 0;
     result.setAttribute("style", "display:block");
+    document.getElementById("btn1").removeAttribute("disabled");
+    document.getElementById("btn2").removeAttribute("disabled");
+    slider.removeAttribute("disabled");
+    mdui.updateSliders();
+    let nums = document.querySelectorAll('#textfield input');
+    for(let[index, num] of nums.entries())
+    {
+        num.removeAttribute("disabled");
+    }
+    mdui.mutation();
+    let sum = 0;
+    for(i = 0; i < Getnum(); i++)
+    {
+        sum += Number(Array[i]);
+    }
+    document.getElementById("listitem1").innerHTML = Array[0];
+    document.getElementById("listitem2").innerHTML = Array[Getnum()-1];
+    document.getElementById("listitem3").innerHTML = Array[1];
+    document.getElementById("listitem4").innerHTML = sum;
+    document.getElementById("listitem5").innerHTML = sum/Getnum();
 }
 
 var Random = function(){
@@ -86,7 +118,7 @@ var Sort = function(){
     {
         for(j = 0; j < Getnum() - 2; j++)
         {
-            if(Array[j] < Array[j+1])
+            if(Number(Array[j]) < Number(Array[j+1]))
             {
                 temp = Array[j];
                 Array[j] = Array[j+1];
